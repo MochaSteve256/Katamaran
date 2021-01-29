6b71eb30004cf7beeb564853c83c29a45d8d4687
Adrian Steyer <adr.st@gmx.de>
2021-01-29 14:51:04 +0100

    Fehler in Server/pos.py behoben und Server/pcalc.py hinzugefügt und programmiert

--- Server/pos.py
+++ Server/pos.py
@@ -1,6 +1,6 @@
-import serial
 import time
 import string
+import serial
 import pynmea2
 import py_qmc5883l
 sensor = py_qmc5883l.QMC5883L()
@@ -8,15 +8,16 @@ def get_gyro():
     m = sensor.get_magnet_raw()
     return m
 def get_loc():
+    global gps
     port="/dev/ttyAMA0"
-	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-	dataout = pynmea2.NMEAStreamReader()
-	newdata=ser.readline()
-	if newdata[0:6] == "$GPRMC":
-		newmsg=pynmea2.parse(newdata)
-		lat=newmsg.latitude
-		lng=newmsg.longitude
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+    if newdata[0:6] == "$GPRMC":
+        newmsg=pynmea2.parse(newdata)
+        lat=newmsg.latitude
+        lng=newmsg.longitude
         alt=newmsg.altidude
         spd=newmsg.spd_over_grnd
-		gps = [str(lat), str(lng), str(alt), str(spd)]
+        gps = [str(lat), str(lng), str(alt), str(spd)]
     return gps
