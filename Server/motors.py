a55d4d16afa164afbaf680c8461fb70dbb0ccdcc
Adrian (Katamaran) <mochasteve@posteo.de>
2024-02-20 18:49:31 +0100

    improved & finished motors.py

--- Server/motors.py
+++ Server/motors.py
@@ -1,31 +1,27 @@
 import pigpio
 
-leftMotor = 4
-rightMotor = 17
+leftMotor = 17
+rightMotor = 4
 
 pi = pigpio.pi()
 pi.set_servo_pulsewidth(leftMotor, 1485)
 pi.set_servo_pulsewidth(rightMotor, 1485)
 
+offset = 1485
 maxValue = 2000
 minValue = 700
 
-def backbord_speed(speed):
-    try:
-        speed = int(speed)
-    except:
-        print("what the hell")
+def backbord_speed(speed: int):
+    speed = -speed
+    speed += offset
     if speed > maxValue:
         speed = maxValue
     elif speed < minValue:
         speed = minValue
     pi.set_servo_pulsewidth(leftMotor, speed)
 
-def steuerbord_speed(speed):
-    try:
-        speed = int(speed)
-    except:
-        print("what the hell")
+def steuerbord_speed(speed: int):
+    speed += offset
     if speed > maxValue:
         speed = maxValue
     elif speed < minValue:
@@ -35,6 +31,16 @@ def steuerbord_speed(speed):
 if __name__ == "__main__":
     while True:
         b = input("backbord_speed: ")
+        try:
+            b = int(b)
+        except:
+            print("what the hell")
+            b = 0
         backbord_speed(b)
         s = input("steuerbord_speed: ")
+        try:
+            s = int(s)
+        except:
+            print("what the hell")
+            s = 0
         steuerbord_speed(s)
