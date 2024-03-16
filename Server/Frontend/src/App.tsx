81213d7394245e52e01ac15387763470a61142a5
Adrian Steyer <adrian.steyer@posteo.de>
2024-03-16 01:29:26 +0100

    servo, net update

--- Server/Frontend/src/App.tsx
+++ Server/Frontend/src/App.tsx
@@ -20,10 +20,10 @@ function App() {
 
     return (
         <Container style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <Slider min={-190} max={190} track={false} value={yValue} onChange={handleYSliderChange} orientation='vertical' style={{ marginRight: '16px', height: '480px' }} />
+            <Slider min={-95} max={95} track={false} value={yValue} onChange={handleYSliderChange} orientation='vertical' style={{ marginRight: '16px', height: '480px' }} />
             <Container style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                <iframe src="http://192.168.178.62:8000/stream.mjpg" width="640" height="480" style={{ border: 'none' }} />
-                <Slider min={-190} max={190} track={false} value={xValue} onChange={handleXSliderChange} style={{ width: '640px', marginTop: '16px' }} />
+                <iframe src="http://192.168.178.62:8000/stream.mjpg" width="640" height="480" style={{ border: 'none' }} loading="lazy" />
+                <Slider min={-95} max={95} track={false} value={xValue} onChange={handleXSliderChange} style={{ width: '640px', marginTop: '16px' }} />
             </Container>
         </Container>
     )
--- Server/net.py
+++ Server/net.py
@@ -7,12 +7,22 @@ import servo
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+x_value = 0
+y_value = 0
+
 @socketio.on('slider_data')
 def handle_slider_data(data):
-    x_value = data.get('x')
-    y_value = data.get('y')
-    servo.setcamx(x_value)
-    servo.setcamy(y_value)
+    x = data.get('x') + 95
+    y = data.get('y') + 95
+    global x_value, y_value
+    if x_value is None or y_value is None:
+        return
+    if x != x_value:
+        x_value = x
+        servo.setcamx(x_value)
+    if y != y_value:
+        y_value = y
+        servo.setcamy(y_value)
 
 if __name__ == '__main__':
     socketio.run(app, use_reloader=True, log_output=True, host='0.0.0.0', port=5000)
