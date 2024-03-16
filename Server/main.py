6bd7ef7e4205b29ad254694903a2d9e5d9475d64
Adrian Steyer <adrian.steyer@posteo.de>
2024-03-16 13:09:49 +0100

    main.py WIP

--- Server/main.py
+++ Server/main.py
@@ -1,17 +1,7 @@
 import net
-import threading
-import os
+import subprocess
 
-def run_camera():
-    os.system("cd /home/pi/Katamaran/Server && python3 camera.py")
-
-camera_thread = threading.Thread(target=run_camera)
-camera_thread.start()
-
-def run_frontend():
-    os.system("cd /home/pi/Katamaran/Server/Frontend && npm run dev")
-
-frontend_thread = threading.Thread(target=run_frontend)
-frontend_thread.start()
+camera_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/camera.py"])
+frontend_process = subprocess.Popen(["npm", "run", "dev"], cwd="/home/pi/Katamaran/Server/Frontend")
 
 net.run()
