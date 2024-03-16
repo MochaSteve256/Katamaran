06b52f9256a15ea86c82b43e43f480ddc66b85b7
Adrian Steyer <adrian.steyer@posteo.de>
2024-03-16 01:19:24 +0100

    net update test

--- Server/net.py
+++ Server/net.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
+import servo
+
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
@@ -9,7 +11,8 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def handle_slider_data(data):
     x_value = data.get('x')
     y_value = data.get('y')
-    print("Received slider data - X:", x_value, "Y:", y_value)
+    servo.setcamx(x_value)
+    servo.setcamy(y_value)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, use_reloader=True, log_output=True)
