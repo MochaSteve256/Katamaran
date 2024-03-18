239be2cb27a14c32c186b971b667e94c0e5b5533
Adrian <adrian.steyer@posteo.de>
2024-03-18 18:29:36 +0100

    main.py testing

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,19 @@
 import subprocess
 import os
-os.system("sudo pigpiod")
-
-import net
-net.run()
-camera_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/camera.py"])
-frontend_process = subprocess.Popen(["npm", "run", "dev"], cwd="/home/pi/Katamaran/Server/Frontend")
+try:
+    os.system("sudo pigpiod")
+except:
+    pass
+try:
+    import net
+    net.run()
+except:
+    pass
+try:
+    camera_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/camera.py"])
+except:
+    pass
+try:
+    frontend_process = subprocess.Popen(["npm", "run", "dev"], cwd="/home/pi/Katamaran/Server/Frontend")
+except:
+    pass
