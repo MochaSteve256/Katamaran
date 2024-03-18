d72c497180ead6be47d8ae85b796414d1dfab099
Adrian <adrian.steyer@posteo.de>
2024-03-18 17:24:50 +0100

    bugfix WIP

--- Server/main.py
+++ Server/main.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 os.system("sudo pigpiod")
 
-net_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/net.py"])
+import net
+net.run()
 camera_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/camera.py"])
 frontend_process = subprocess.Popen(["npm", "run", "dev"], cwd="/home/pi/Katamaran/Server/Frontend")
