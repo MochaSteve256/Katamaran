550de476ec0615cf1db7445cf4b5c33c14ab737f
Adrian <adrian.steyer@posteo.de>
2024-03-18 15:45:48 +0100

    put net into subprocess

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,7 @@
-import net
 import subprocess
+import os
+os.system("sudo pigpio")
 
+net_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/net.py"])
 camera_process = subprocess.Popen(["python3", "/home/pi/Katamaran/Server/camera.py"])
 frontend_process = subprocess.Popen(["npm", "run", "dev"], cwd="/home/pi/Katamaran/Server/Frontend")
-
-net.run()
